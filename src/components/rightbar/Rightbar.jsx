97d18cec42fb954de218ceef419cdf9415237ec6
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-03-28 15:18:51 +0400

    Rightbar complated

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,33 @@
-import "./rightbar.css"
+import "./rightbar.css";
 
 const Rightbar = () => {
   return (
-    <div className="rightbar">Rightbar</div>
-  )
-}
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="/assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">Vito Scaletta</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Rightbar
+export default Rightbar;
